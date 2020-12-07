c3806f0ea0b4ebe120137bbf482d62262b199b17
reika178 <rechiko178@gmail.com>
2020-12-08 08:01:49 +0900

    8途中 12/8

--- lib/shopping_cart_tab.dart
+++ lib/shopping_cart_tab.dart
@@ -11,6 +11,40 @@ class ShoppingCartTab extends StatefulWidget {
 }
 
 class _ShoppingCartTabState extends State<ShoppingCartTab> {
+  String name;
+  String email;
+  String location;
+  String pin;
+  DateTime dateTime = DateTime.now();
+
+  Widget _buildNameField() {
+    return CupertinoTextField(
+      prefix: const Icon(
+        CupertinoIcons.person_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      textCapitalization: TextCapitalization.words,
+      autocorrect: false,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Name',
+      onChanged: (newName) {
+        setState(() {
+          name = newName;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
