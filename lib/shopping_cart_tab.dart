51d8b77fe1e8fdb96546a18bb8b2fe523609bf98
reika178 <rechiko178@gmail.com>
2020-12-09 07:27:04 +0900

    8終了
    12/9

--- lib/shopping_cart_tab.dart
+++ lib/shopping_cart_tab.dart
@@ -45,15 +45,95 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
     );
   }
 
+  Widget _buildEmailField() {
+    return const CupertinoTextField(
+      prefix: const Icon(
+        CupertinoIcons.mail_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Email',
+    );
+  }
+
+  Widget _buildLocationField() {
+    return const CupertinoTextField(
+      prefix: const Icon(
+        CupertinoIcons.location_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      textCapitalization: TextCapitalization.words,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Location',
+    );
+  }
+
+  SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
+    AppStateModel model) {
+      return SliverChildBuilderDelegate(
+        (context, index) {
+          switch (index) {
+            case 0:
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: _buildNameField(),
+              );
+            case 1:
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: _buildEmailField(),
+              );
+            case 2:
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: _buildLocationField(),
+              );
+            default:
+
+          }
+          return null;
+        }
+      );
+    }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
       builder: (context, model, child) {
-        return const CustomScrollView(
+        return CustomScrollView(
           slivers: <Widget>[
-            CupertinoSliverNavigationBar(
+            const CupertinoSliverNavigationBar(
               largeTitle: Text('Shopping Cart'),
             ),
+            SliverSafeArea(
+              top: false,
+              minimum: const EdgeInsets.only(top: 4),
+              sliver: SliverList(
+                delegate: _buildSliverChildBuilderDelegate(model),
+              ),
+            )
           ],
         );
       },
